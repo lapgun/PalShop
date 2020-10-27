a8b3287b1916045eee99c570fcd9aaf4190790c9
lapph <lapph@vnext.com.vn>
2020-10-27 18:01:36 +0700

    fix: update layout admin

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -20,7 +20,10 @@
                     <div class="container-fluid">
                         <div class="card shadow mb-4">
                             <div class="card-header py-3">
-                                <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                                <h6 class="m-0 font-weight-bold text-primary">Management User</h6>
+                            </div>
+                            <div class="add-item">
+                                <button class="btn btn-success mt-3 ml-3" data-toggle="modal" data-target="#exampleModal" style="color:#ffffff"><i class="fas fa-plus"></i></button>
                             </div>
                             <div class="card-body">
                                 <div class="table-responsive">
@@ -74,6 +77,39 @@
                                 </div>
                             </div>
                         </div>
+
+                        {{--modal--}}
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">New message</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form>
+                                            <div class="form-group">
+                                                <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                <input type="text" class="form-control" id="recipient-name">
+                                            </div>
+                                            <div class="form-group">
+                                                <label for="message-text" class="col-form-label">Message:</label>
+                                                <textarea class="form-control" id="message-text"></textarea>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Send message</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        {{--endModal--}}
+
 
                         <div id="content" class="col-lg-12">
                             <div class="card">
